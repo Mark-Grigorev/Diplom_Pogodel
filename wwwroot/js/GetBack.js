6df0e293d88881be9afbf10aed1c6d6dc58e5a05
Mark Grigorev <107909196+Mark-Grigorev@users.noreply.github.com>
2023-08-23 21:03:55 +0600

    GetBack.js

--- wwwroot/js/GetBack.js
+++ wwwroot/js/GetBack.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     // Обработчик события нажатия кнопки
     $("#goBackButton").click(function () {
-        window.location.href = "/Weather/RequestFormPage"; // Замените "ControllerName" и "ActionName" на соответствующие значения вашего контроллера и действия
+        window.location.href = "/Weather/RequestFormPage";
     });
 });
